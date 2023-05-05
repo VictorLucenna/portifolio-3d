df2813e0ff56c5160bf8f4c74c95b1f2fd8dd5a2
VictorLucenna <andvtor@gmail.com>
2023-05-05 00:37:22 -0300

    Update index.jsx

--- src/paginas/Inicio/index.jsx
+++ src/paginas/Inicio/index.jsx
@@ -4,56 +4,58 @@ import styles from './Inicio.module.css'
 
 export default function Inicio() {
   return (
-    <div className={styles.galeria}>
-      <div className={styles.card}>
-        <img className={styles.fold} src={'assets/posts/01/capa.jpg'} alt="Foto da capa" />
-      </div>
-      <div className={styles.card}>
-        <img className={styles.fold} src={'assets/posts/01/capa.jpg'} alt="Foto da capa" />
-      </div>
-      <div className={styles.card}>
-        <img className={styles.fold} src={'assets/posts/01/capa.jpg'} alt="Foto da capa" />
-      </div>
-      <div className={styles.card}>
-        <img className={styles.fold} src={'assets/posts/01/capa.jpg'} alt="Foto da capa" />
-      </div>
-      <div className={styles.card}>
-        <img className={styles.fold} src={'assets/posts/01/capa.jpg'} alt="Foto da capa" />
-      </div>
-      <div className={styles.card}>
-        <img className={styles.fold} src={'assets/posts/01/capa.jpg'} alt="Foto da capa" />
-      </div>
-      <div className={styles.card}>
-        <img className={styles.fold} src={'assets/posts/01/capa.jpg'} alt="Foto da capa" />
-      </div>
-      <div className={styles.card}>
-        <img className={styles.fold} src={'assets/posts/01/capa.jpg'} alt="Foto da capa" />
-      </div>
-      <div className={styles.card}>
-        <img className={styles.fold} src={'assets/posts/01/capa.jpg'} alt="Foto da capa" />
-      </div>
-      <div className={styles.card}>
-        <img className={styles.fold} src={'assets/posts/01/capa.jpg'} alt="Foto da capa" />
-      </div>
-      <div className={styles.card}>
-        <img className={styles.fold} src={'assets/posts/01/capa.jpg'} alt="Foto da capa" />
-      </div>
-      <div className={styles.card}>
-        <img className={styles.fold} src={'assets/posts/01/capa.jpg'} alt="Foto da capa" />
-      </div>
-      <div className={styles.card}>
-        <img className={styles.fold} src={'assets/posts/01/capa.jpg'} alt="Foto da capa" />
-      </div>
-      <div className={styles.card}>
-        <img className={styles.fold} src={'assets/posts/01/capa.jpg'} alt="Foto da capa" />
-      </div>
-      <div className={styles.card}>
-        <img className={styles.fold} src={'assets/posts/01/capa.jpg'} alt="Foto da capa" />
-      </div>
-      <div className={styles.card}>
-        <img className={styles.fold} src={'assets/posts/01/capa.jpg'} alt="Foto da capa" />
-      </div>
+    <div className={styles.container}>
+      <div className={styles.galeria}>
+        <div className={styles.card}>
+          <img className={styles.fold} src={'assets/posts/01/capa.jpg'} alt="Foto da capa" />
+        </div>
+        <div className={styles.card}>
+          <img className={styles.fold} src={'assets/posts/01/capa.jpg'} alt="Foto da capa" />
+        </div>
+        <div className={styles.card}>
+          <img className={styles.fold} src={'assets/posts/01/capa.jpg'} alt="Foto da capa" />
+        </div>
+        <div className={styles.card}>
+          <img className={styles.fold} src={'assets/posts/01/capa.jpg'} alt="Foto da capa" />
+        </div>
+        <div className={styles.card}>
+          <img className={styles.fold} src={'assets/posts/01/capa.jpg'} alt="Foto da capa" />
+        </div>
+        <div className={styles.card}>
+          <img className={styles.fold} src={'assets/posts/01/capa.jpg'} alt="Foto da capa" />
+        </div>
+        <div className={styles.card}>
+          <img className={styles.fold} src={'assets/posts/01/capa.jpg'} alt="Foto da capa" />
+        </div>
+        <div className={styles.card}>
+          <img className={styles.fold} src={'assets/posts/01/capa.jpg'} alt="Foto da capa" />
+        </div>
+        <div className={styles.card}>
+          <img className={styles.fold} src={'assets/posts/01/capa.jpg'} alt="Foto da capa" />
+        </div>
+        <div className={styles.card}>
+          <img className={styles.fold} src={'assets/posts/01/capa.jpg'} alt="Foto da capa" />
+        </div>
+        <div className={styles.card}>
+          <img className={styles.fold} src={'assets/posts/01/capa.jpg'} alt="Foto da capa" />
+        </div>
+        <div className={styles.card}>
+          <img className={styles.fold} src={'assets/posts/01/capa.jpg'} alt="Foto da capa" />
+        </div>
+        <div className={styles.card}>
+          <img className={styles.fold} src={'assets/posts/01/capa.jpg'} alt="Foto da capa" />
+        </div>
+        <div className={styles.card}>
+          <img className={styles.fold} src={'assets/posts/01/capa.jpg'} alt="Foto da capa" />
+        </div>
+        <div className={styles.card}>
+          <img className={styles.fold} src={'assets/posts/01/capa.jpg'} alt="Foto da capa" />
+        </div>
+        <div className={styles.card}>
+          <img className={styles.fold} src={'assets/posts/01/capa.jpg'} alt="Foto da capa" />
+        </div>
 
+      </div>
     </div>
   )
 }
