b5ad461ca6fd9ae8996f0cbabf8e406420933e46
VictorLucenna <andvtor@gmail.com>
2023-05-25 18:07:25 -0300

    updates

--- src/paginas/AboutMe/index.jsx
+++ src/paginas/AboutMe/index.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import styles from './AboutMe.module.css'
+import Container from '../../componentes/Container'
 
 export default function AboutMe() {
   return (
-    <div>AboutMe</div>
+    <Container>
+      <div>
+        <img src="" alt="My PT" />
+      </div>
+      <div>
+        <p>
+          <p>Olá a todos!
+          </p>
+          <p>Meu nome é Victor e sou um modelador 3D apaixonado por arte.
+          </p>
+          <p>Curioso e sempre em busca de novos desafios. Com uma paixão incansável por explorar os limites da criatividade, busco constantemente expandir meus conhecimentos e habilidades na área de modelagem 3D.
+          </p>
+          <p> Desde cedo, descobri que minha verdadeira vocação estava em transformar ideias abstratas em belas representações tridimensionais. Através da modelagem 3D, posso dar vida a conceitos e imagens, criando um universo virtual que cativa e inspira.
+          </p>
+          <p> Minha curiosidade insaciável impulsiona-me a buscar sempre novas técnicas, tecnologias e tendências emergentes na indústria. Gosto de me desafiar a aprender coisas novas e aprimorar minha expertise em softwares de modelagem 3D de última geração. Estou sempre aberto a novos projetos que me permitam explorar diferentes estilos e abordagens, expandindo minha visão artística.
+          </p>
+          <p> Além da modelagem 3D, tenho interesse em áreas relacionadas, como animação, ilustração e design gráfico. Acredito que essa abordagem multidisciplinar enriquece meu trabalho, permitindo-me criar experiências visuais mais completas e imersivas.
+          </p>
+          <p> Neste site, você encontrará uma seleção dos meus melhores projetos, nos quais dediquei tempo e dedicação para alcançar resultados excepcionais. Cada modelo 3D que criei é uma peça única, cuidadosamente elaborada para transmitir mensagens e contar histórias de forma visualmente impactante.
+          </p>
+          <p> Convido você a explorar meu portfólio e mergulhar no meu mundo de criações 3D. Se você estiver procurando por um modelador 3D curioso, apaixonado por arte e sempre em busca de novos desafios, estou pronto para colaborar e transformar suas ideias em realidade.
+          </p>
+          <p> Obrigado por visitar meu site e espero poder trabalhar com você em um projeto emocionante em breve.
+          </p>
+        </p>
+      </div>
+    </Container>
   )
 }
